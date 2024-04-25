da81c1dc4baf00131200bcce85df8d771a9374f0
Davi-RD <davirdavanzo@gmail.com>
2024-04-25 09:19:56 -0300

    Migração do calendário para uma view

---
+++ frontend/src/views/CalendarView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="top-bar">
+      <button @click="setMonth(-1)"><</button>
+      {{ monthName }}
+      <button @click="setMonth(1)">></button>
+    </div>
+    <div class="absolute top-16 left-0 w-full h-full bg-white text-black">
+      <ul class="flex flex-wrap">
+        <li class="font-bold day" v-for="index in mounthDays" :key="index">
+          <button class="day">{{ index }}</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarView',
+  mounted() {
+    this.setMonth(0)
+  },
+  data() {
+    return {
+      date: new Date(),
+      monthName: '',
+      mounthDays: 0
+    }
+  },
+  methods: {
+    setMonth(increment) {
+      this.date.setMonth(this.date.getMonth() + increment)
+      this.monthName = this.date.toLocaleString('pt-BR', { month: 'long' }).toUpperCase()
+      this.mounthDays = new Date(2024, this.date.getMonth() + 1, 0).getDate()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.day {
+  border: 1px solid #1a202c;
+  height: 20vh;
+  width: 20vh;
+  font-size: 8vw;
+}
+
+.day:hover {
+  transform: scale(1.1);
+  border: 0px;
+}
+</style>
