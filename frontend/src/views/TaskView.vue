ec035da4dea035afc75ab0cf43fc5831e83db152
Davi-RD <davirdavanzo@gmail.com>
2024-04-25 17:16:18 -0300

    Continuação da tela de tarefas

--- frontend/src/views/TaskView.vue
+++ frontend/src/views/TaskView.vue
@@ -2,10 +2,17 @@
   <div>
     <div class="top-bar">{{ date }}</div>
 
-    <div class="background items-center justify-center flex text-black">
+    <div class="background items-center justify-center flex flex-col text-black">
       <input class="task-input text-black" v-model="task" @keypress.enter="addTask()" />
       <ul>
-        <li v-for="task in tasks" :key="task">{{ task }}</li>
+        <li
+          class="task text-white mt-4 font-bold grid grid-cols-3 gap-4"
+          v-for="(task, index) in tasks"
+          :key="task"
+        >
+          <span class="col-span-3">{{ task }}</span>
+          <button class="col-start-4" @click="deleteTask(index)">D</button>
+        </li>
       </ul>
     </div>
   </div>
@@ -30,6 +37,9 @@ export default {
     addTask() {
       this.tasks.push(this.task)
       this.task = ''
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
     }
   }
 }
@@ -41,5 +51,13 @@ export default {
   width: 30vh;
   border: 5px solid #1a202c;
   border-radius: 20px;
+}
+
+.task {
+  min-height: 5vh;
+  min-width: 40vh;
+  background-color: #1a202c;
+  border-radius: 10px;
+  padding: 10px;
 }
 </style>
