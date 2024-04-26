27c8ed5ac118b39cc619ddc820ee1cdb9f701939
Davi-RD <davirdavanzo@gmail.com>
2024-04-26 09:21:05 -0300

    Adicão dos botões voltar e adicionar tarefa  na top bar

--- frontend/src/views/TaskView.vue
+++ frontend/src/views/TaskView.vue
@@ -1,34 +1,46 @@
 <template>
   <div>
-    <div class="top-bar">{{ date }}</div>
+    <div class="top-bar">
+      <button @click="returnToCalendar()"><</button>
+      {{ date }}
+      <button>+</button>
+    </div>
 
     <div class="background items-center justify-center flex flex-col text-black">
       <input class="task-input text-black" v-model="task" @keypress.enter="addTask()" />
-      <ul>
-        <li
-          class="task text-white mt-4 font-bold grid grid-cols-3 gap-4"
-          v-for="(task, index) in tasks"
-          :key="task"
-        >
-          <span class="col-span-3">{{ task }}</span>
-          <button class="col-start-4" @click="deleteTask(index)">D</button>
-        </li>
-      </ul>
+      <task-item v-for="(task, index) in tasks" :key="task" :task="task" :index="index" />
     </div>
   </div>
 </template>
 
 <script>
 import { useRoute } from 'vue-router'
+import TaskItem from '@/components/TaskItem.vue'
+import router from '../router'
 import moment from 'moment'
 export default {
+  components: { TaskItem },
   mounted() {
     this.date = moment(this.route.params.date).format('DD/MM/YYYY')
   },
   data() {
     return {
       task: '',
-      tasks: [],
+      tasks: [
+        {
+          title: 'tarefa1',
+          description: 'Realizar a primeira tarefa no prazo adequado',
+          date: { time: '17:00' },
+          duration: '01:30'
+        },
+        {
+          title: 'Segunda Tarefa',
+          description:
+            'Testando a capacidade do componente tarefa de manter o design adequado mesmo com descrições grandes',
+          date: { time: '17:00' },
+          duration: '01:30'
+        }
+      ],
       route: useRoute(),
       date: ''
     }
@@ -40,6 +52,9 @@ export default {
     },
     deleteTask(index) {
       this.tasks.splice(index, 1)
+    },
+    returnToCalendar() {
+      router.push('/')
     }
   }
 }
@@ -51,13 +66,5 @@ export default {
   width: 30vh;
   border: 5px solid #1a202c;
   border-radius: 20px;
-}
-
-.task {
-  min-height: 5vh;
-  min-width: 40vh;
-  background-color: #1a202c;
-  border-radius: 10px;
-  padding: 10px;
 }
 </style>
